315b1bf2cb2c4ac2fbc7226fc59b1a20601e69cf
Jacob Ajit <jacobvajit@gmail.com>
2021-01-16 03:46:00 -0500

    can't accurately use eval7 evaluate to evaluate incomplete hands against each other

--- gamelogParser.py
+++ gamelogParser.py
@@ -9,6 +9,7 @@ import eval7
 GAME_LOG_FILE = 'game_log.txt'
 A = "A"
 B = "B"
+_MONTE_CARLO_ITERS = 100
 
 f = open(GAME_LOG_FILE)
 loglines = f.readlines()
@@ -42,6 +43,61 @@ class Round:
 		return str(self.__class__) + ": " + str(self.__dict__)
 
 rounds_data = []
+
+def calculate_strength(hole_cards, community_cards, iters):
+	'''
+    A Monte Carlo method meant to estimate the win probability of a pair of
+    hole cards. Simlulates 'iters' games and determines the win rates of our cards
+
+    Arguments:
+    hole: a list of our two hole cards
+    iters: a integer that determines how many Monte Carlo samples to take
+    '''
+
+	deck = eval7.Deck()  # eval7 object!
+	hole_cards = [eval7.Card(card) for card in hole_cards]  # card objects, used to evaliate hands
+	community_cards = [eval7.Card(card) for card in community_cards]  # card objects, used to evaliate hands
+
+	for card in hole_cards:  # remove cards that we know about! they shouldn't come up in simulations
+		deck.cards.remove(card)
+
+	for card in community_cards:  # remove cards that we know about! they shouldn't come up in simulations
+		deck.cards.remove(card)
+
+	score = 0
+
+	for _ in range(iters):  # take 'iters' samples
+		deck.shuffle()  # make sure our samples are random
+
+		_COMM = 5 - len(community_cards)  # the number of cards we need to draw
+		_OPP = 2
+
+		draw = deck.peek(_COMM + _OPP)
+
+		opp_hole = draw[: _OPP]
+		hidden_community = draw[_OPP:]
+
+		our_hand = hole_cards + community_cards + hidden_community  # the two showdown hands
+		opp_hand = opp_hole + community_cards + hidden_community
+
+		our_hand_value = eval7.evaluate(our_hand)  # the ranks of our hands (only useful for comparisons)
+		opp_hand_value = eval7.evaluate(opp_hand)
+
+		if our_hand_value > opp_hand_value:  # we win!
+			score += 2
+
+		elif our_hand_value == opp_hand_value:  # we tie.
+			score += 1
+
+		else:  # we lost....
+			score += 0
+
+	hand_strength = score / (2 * iters)  # this is our win probability!
+
+
+	# print("sampling duration", sampling_duration)
+
+	return hand_strength
 
 def parse_hand(line):
 	return line[line.index("[") + 1:line.index("]")].split()
@@ -130,8 +186,18 @@ while i < len(loglines):
 				player = line_arr[0]
 				other_player = B if player == A else A
 				board = int(line_arr[-1])
-				winner, a_hand_desc, b_hand_desc = eval_hands(round_pkr.boards[board].A_holes, round_pkr.boards[board].B_holes,
-						   round_pkr.boards[board].community_cards)
+
+				a_strength = calculate_strength(round_pkr.boards[board].A_holes, round_pkr.boards[board].community_cards, _MONTE_CARLO_ITERS)
+				b_strength = calculate_strength(round_pkr.boards[board].B_holes,
+												round_pkr.boards[board].community_cards, _MONTE_CARLO_ITERS)
+				if a_strength > b_strength:
+					winner = A
+				elif b_strength > a_strength:
+					winner = B
+				else:
+					winner = "TIE"
+				# winner, a_hand_desc, b_hand_desc = eval_hands(round_pkr.boards[board].A_holes, round_pkr.boards[board].B_holes,
+				# 		   round_pkr.boards[board].community_cards)
 				a_had_better = (A == winner)
 				tie = (winner == "TIE")
 
